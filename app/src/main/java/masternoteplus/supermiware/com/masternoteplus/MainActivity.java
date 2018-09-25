2b717b968bd0eecd0b7e461ab3f5050013864ab2
Hamzah <supermiware@gmail.com>
2018-09-25 01:39:47 -0500

    This is our application master note plus

--- app/src/main/java/masternoteplus/supermiware/com/masternoteplus/MainActivity.java
+++ app/src/main/java/masternoteplus/supermiware/com/masternoteplus/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     private String textField2String;
 
     private Button noteFeed;
+    // this is a commit note
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
